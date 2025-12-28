fe168ab7516fdd91332ce8d24fe3da26f96915b7
safin-sys <mdsafinofficial@gmail.com>
2025-12-28 06:46:02 +0600

    refactor: improvement to generate_token function

--- backend/src/utils/jwt.tsx
+++ backend/src/utils/jwt.tsx
@@ -17,17 +17,13 @@ const generate_token = async (
     const jwt_payload: JWTPayload = {
         id: payload.id,
         role: payload.role,
+        type,
         exp: payload.exp ?? Math.floor(Date.now() / 1000) + 60 * 5, // Token expires in 5 minutes
         iat: payload.iat ?? Math.floor(Date.now() / 1000),
         data: payload.data,
     }
 
-    switch (type) {
-        case "access":
-            return await sign(jwt_payload, secret)
-        case "refresh":
-            return await sign(jwt_payload, secret)
-    }
+    return await sign(jwt_payload, secret)
 }
 
 export default generate_token;

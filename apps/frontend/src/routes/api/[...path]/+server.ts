9d4dd3cedf50b35fab9ac084ed2b1e2ce92f6350
safin-sys <mdsafinofficial@gmail.com>
2026-04-01 09:56:28 +0600

    this better work

---
+++ apps/frontend/src/routes/api/[...path]/+server.ts
@@ -0,0 +1,21 @@
+import type { RequestHandler } from './$types';
+import { PUBLIC_API_URL } from '$env/static/public';
+
+const handler: RequestHandler = async ({ request, params }) => {
+    const url = new URL(request.url);
+    const targetUrl = `${PUBLIC_API_URL}/${params.path}${url.search}`;
+
+    return fetch(targetUrl, {
+        method: request.method,
+        headers: request.headers,
+        body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : undefined,
+        // @ts-ignore
+        duplex: 'half'
+    });
+};
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
+export const DELETE = handler;
+export const OPTIONS = handler;

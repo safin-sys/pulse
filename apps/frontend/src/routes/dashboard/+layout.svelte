923c7dd61e0a564319385bd00197ad35e1fe2ac2
safin-sys <mdsafinofficial@gmail.com>
2026-04-16 00:51:07 +0600

    feat: dashboard title added

--- apps/frontend/src/routes/dashboard/+layout.svelte
+++ apps/frontend/src/routes/dashboard/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import Header from "$lib/components/dashboard/header/index.svelte";
 	import FloatingNav from "$lib/components/floating-nav.svelte";
+	import { projects } from "$lib/stores/projects.svelte";
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<title>{projects.selected_project?.name || "Orbit"} | Dashboard</title>
+</svelte:head>
 
 <div class="h-screen overflow-x-hidden bg-background font-sans text-foreground">
 	<Header />

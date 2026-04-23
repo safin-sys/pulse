e32c97aae814b6da3a51e2d2e2ef6521d8f81c6a
safin-sys <mdsafinofficial@gmail.com>
2026-04-23 23:52:28 +0600

    feat: orbit is tracking orbit, orbitseption lol

--- apps/frontend/src/routes/+layout.svelte
+++ apps/frontend/src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import { fade } from 'svelte/transition';
-	import { init } from '$lib/stores/auth.svelte';
-	import { beforeNavigate } from '$app/navigation';
-	import { dashboard, reset_dashboard } from '$lib/stores/dashboard.svelte';
-	import { onMount } from 'svelte';
-	import { Toaster } from '$lib/components/ui/sonner';
+	import "./layout.css";
+	import favicon from "$lib/assets/favicon.svg";
+	import { fade } from "svelte/transition";
+	import { init } from "$lib/stores/auth.svelte";
+	import { beforeNavigate } from "$app/navigation";
+	import { dashboard, reset_dashboard } from "$lib/stores/dashboard.svelte";
+	import { onMount } from "svelte";
+	import { Toaster } from "$lib/components/ui/sonner";
 
 	let { children } = $props();
 
@@ -15,13 +15,20 @@
 	});
 
 	beforeNavigate((nav) => {
-		if (dashboard.demo && nav.to?.url && !nav.to.url.pathname.startsWith('/demo')) {
+		if (dashboard.demo && nav.to?.url && !nav.to.url.pathname.startsWith("/demo")) {
 			reset_dashboard();
 		}
 	});
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<script
+		src="https://orbit.pages.dev/orbit.js"
+		data-api-key="orb_a8687b3acf3f9eedeead5e3e42ee14dff55e94b1ca822cfd124b37a28e22391a"
+	></script>
+</svelte:head>
+
 <Toaster />
 <div in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
 	{@render children()}
